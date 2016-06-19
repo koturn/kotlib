5939d5722961f38f4402d5986fb3d9c2a5c04ae6
koturn <jeak.koutan.apple@gmail.com>
2016-06-20 02:41:24 +0900

    Fix identifing MSVC version

--- include/kotlib/compat/cxx11.h
+++ include/kotlib/compat/cxx11.h
@@ -16,28 +16,53 @@
 
 #if KOTLIB_COMPAT_IS_SUPPORT_C11
 #  define  static_assert(expr, msg)  _Static_assert(expr, msg)
-#elif _MSC_VER < 1600 || \
+#elif defined(_MSC_VER) && _MSC_VER < 1600 || \
   !(KOTLIB_COMPAT_IS_SUPPORT_CXX11 && \
     (KOTLIB_COMPAT_GNUC_PREREQ(4, 3) || KOTLIB_COMPAT_CLANG_PREREQ(2, 9)))
-#  define static_assert(expr, msg) \
-     typedef char KOTLIB_STATIC_ASSERT_ARRAY__[(msg, expr) ? 1 : -1]
+#  ifdef __COUNTER__
+#    define KOTLIB_COMPAT_STATIC_ASSERT_ARRAY__ \
+       KOTLIB_COMPAT_STATIC_ASSERT_CONCAT_MACRO(KOTLIB_COMPAT_STATIC_ASSERT_ARRAY, _, __LINE__, _, __COUNTER__, __)
+#    define KOTLIB_COMPAT_STATIC_ASSERT_CONCAT_MACRO(arg1, arg2, arg3, arg4, arg5, arg6) \
+       KOTLIB_COMPAT_STATIC_ASSERT_CONCAT_MACRO_(arg1, arg2, arg3, arg4, arg5, arg6)
+#    define KOTLIB_COMPAT_STATIC_ASSERT_CONCAT_MACRO_(arg1, arg2, arg3, arg4, arg5, arg6) \
+       KOTLIB_COMPAT_STATIC_ASSERT_CONCAT_MACRO__(arg1, arg2, arg3, arg4, arg5, arg6)
+#    define KOTLIB_COMPAT_STATIC_ASSERT_CONCAT_MACRO__(arg1, arg2, arg3, arg4, arg5, arg6) \
+       arg1 ## arg2 ## arg3 ## arg4 ## arg5 ## arg6
+#  else
+#    define KOTLIB_COMPAT_STATIC_ASSERT_ARRAY__ \
+       KOTLIB_COMPAT_STATIC_ASSERT_CONCAT_MACRO(KOTLIB_COMPAT_STATIC_ASSERT_ARRAY, _, __LINE__, __)
+#    define KOTLIB_COMPAT_STATIC_ASSERT_CONCAT_MACRO(arg1, arg2, arg3, arg4) \
+       KOTLIB_COMPAT_STATIC_ASSERT_CONCAT_MACRO_(arg1, arg2, arg3, arg4)
+#    define KOTLIB_COMPAT_STATIC_ASSERT_CONCAT_MACRO_(arg1, arg2, arg3, arg4) \
+       KOTLIB_COMPAT_STATIC_ASSERT_CONCAT_MACRO__(arg1, arg2, arg3, arg4)
+#    define KOTLIB_COMPAT_STATIC_ASSERT_CONCAT_MACRO__(arg1, arg2, arg3, arg4) \
+       arg1 ## arg2 ## arg3 ## arg4
+#  endif
+#  define KOTLIB_STATIC_ASSERT_ARRAY  KOTLIB_COMPAT_STATIC_ASSERT_ARRAY__
+#  ifdef __cplusplus
+#    define static_assert(expr, msg) \
+       typedef char KOTLIB_STATIC_ASSERT_ARRAY[(expr) ? 1 : -static_cast<int>(sizeof(msg))];
+#  else
+#    define static_assert(expr, msg) \
+       typedef char KOTLIB_STATIC_ASSERT_ARRAY[(expr) ? 1 : -(int) sizeof(msg)];
+#  endif
 #endif
 
 
-#if _MSC_VER < 1600 || \
+#if defined(_MSC_VER) && _MSC_VER < 1600 || \
   !(KOTLIB_COMPAT_IS_SUPPORT_CXX11 && \
     (KOTLIB_COMPAT_GNUC_PREREQ(4, 6) || KOTLIB_COMPAT_CLANG_PREREQ(3, 0)))
 #  define nullptr  NULL
 #endif
 
 
-#if _MSC_VER < 1700 || !KOTLIB_COMPAT_IS_SUPPORT_CXX11
+#if defined(_MSC_VER) && _MSC_VER < 1700 || !KOTLIB_COMPAT_IS_SUPPORT_CXX11
 #  define final
 #  define override
 #endif
 
 
-#if _MSC_VER < 1800 || \
+#if defined(_MSC_VER) && _MSC_VER < 1800 || \
   !(KOTLIB_COMPAT_IS_SUPPORT_CXX11 && KOTLIB_COMPAT_GNUC_PREREQ(4, 6))
 #  define noexcept
 #endif
@@ -51,8 +76,8 @@
 
 
 #if !(KOTLIB_COMPAT_IS_SUPPORT_CXX11 && \
-  KOTLIB_COMPAT_GNUC_PREREQ(4, 8) || KOTLIB_COMPAT_CLANG_PREREQ(3, 3))
-#  if 1600 <= _MSC_VER && _MSC_VER < 1800
+  (KOTLIB_COMPAT_GNUC_PREREQ(4, 8) || KOTLIB_COMPAT_CLANG_PREREQ(3, 3)))
+#  if defined(_MSC_VER) && 1600 <= _MSC_VER && _MSC_VER < 1800
 #    define thread_local  __declspec(thread)
 #  elif defined(__GNUC__)
 #    define thread_local  __thread
@@ -64,7 +89,7 @@
 
 #if !(KOTLIB_COMPAT_IS_SUPPORT_CXX11 && \
   (KOTLIB_COMPAT_GNUC_PREREQ(4, 8) || KOTLIB_COMPAT_CLANG_PREREQ(3, 3)))
-#  if 1300 <= _MSC_VER && _MSC_VER < 1800
+#  if defined(_MSC_VER) && 1300 <= _MSC_VER && _MSC_VER < 1800
 #    define alignas(n)  __declspec(align(n))
 #  elif defined(__GNUC__)
 #    define alignas(n)  __attribute__((aligned(n)))
@@ -74,12 +99,12 @@
 #endif
 
 
-#if 1300 <= _MSC_VER && _MSC_VER < 1800 || \
+#if defined(_MSC_VER) && 1300 <= _MSC_VER && _MSC_VER < 1800 || \
   !KOTLIB_COMPAT_IS_SUPPORT_CXX11 && defined(__GNUC__)
 #  define alignof(n)  __alignof(n)
 #endif
 
 
-#include "undefsupport.h"
+// #include "undefsupport.h"
 
 #endif  // KOTLIB_COMPAT_CXX11_H
